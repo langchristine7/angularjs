527da8f6da987f2c6086a8d5ccbb173b99d8b05e
Christine stras <lang.christine7@gmail.com>
2015-11-18 15:13:24 +0100

    selection avec cle

--- ingoApp/js/controllers/mainCtrl.js
+++ ingoApp/js/controllers/mainCtrl.js
@@ -16,11 +16,39 @@ ingoControllers.controller('ingoListCtrl',['$scope', '$http', function($scope, $
           //  voir site http://underscorejs.org
       // $scope.ingos = data; // avant de filter les Cles
       $scope.ingosInit = data;
-      $scope.ingos = _.map(data, function(ngo)) {
-
-      };
-
-
+      $scope.ingos = _.map(data, function(ingo) {
+          return {
+            nameEn:ingo["Name (english)"],
+            acronymEn:ingo["Acronym (english)"],
+            nameFr:ingo["Name (french)"],
+            acronymFr:ingo["Acronym (French)"],
+            street:ingo["Street"],
+            postcode:ingo["Postcode"],
+            town:ingo["Town"],
+            country:ingo["Country"],
+            tel:ingo["Tel"],
+            fax:ingo["Fax"],
+            website:ingo["Website"],
+            email:ingo["Email"],
+            funding:ingo["Funding"],
+            nature:ingo["Nature of organisation"],
+            presidentFirstName:ingo["President First Name"],
+            presidentLastName:ingo["President Last Name"],
+            presidentAddress:ingo["President Address"],
+            sgFirstName:ingo["SG First Name"],
+            sgLastName:ingo["SG Last Name"],
+            sgAddress:ingo["SG Address"],
+            yearOfFoundation:ingo["Year of foundation"],
+            staff:ingo["Staff"],
+            numberOfMembers:ingo["Number of members"],
+            workingLanguages:ingo["Working languages"],
+            officialLanguages:ingo["Official languages"],
+            committees:ingo["Committes"],
+            geoReps:ingo["Geographical representation"],
+            aocs:ingo["Areas of competence"],
+            yearStatus:ingo["yearStatus"]
+          };
+      });
     })
     .error(function(){
       console.log ("pb lecture fichier js/ngos.json");
