ad25d539f14c84e8f8d28a3691853c15ab7c96e1
Clang mb <lang.christine7@gmail.com>
2015-11-22 08:25:27 +0100

    fin du code
    code de vendredi soir
    celui d’angelique

--- ingoApp/js/app.js
+++ ingoApp/js/app.js
@@ -1,81 +1,96 @@
 var app = angular.module('ingoApp', ['ingoControllers', 'ngRoute', 'ngSanitize', 'angular.filter']);
 
-
-/* Routes */
-app.config(['$routeProvider', function($routeProvider) {
-    
-    $routeProvider.when('/', {
-            templateUrl: 'partials/home.html'
-        })
-        .when('/ingos', {
-            templateUrl: 'partials/ingoListView.html',
-            controller: 'ingoListCtrl'
-        })
-        .when('/ingo/:id', {
-            templateUrl: 'partials/ingoDetailView.html',
-            controller: 'ingoDetailCtrl'
-        })
-        .when('/sign-in', {
-            templateUrl: 'partials/ingoSignInView.html',
-            controller: 'ingoSignInCtrl'
-        })
-        .otherwise({
-            redirectTo: '/'
-        })
-    
+app.config(['$routeProvider', function($routeProvider){
+        
+	// ce qu'il doit faire quand on se trouve dans telle ou telle url
+	$routeProvider
+		.when('/', {
+			templateUrl: 'partials/home.html'
+		})
+		.when('/ingos', {
+			templateUrl: 'partials/ingoListView.html',
+			controller: 'ingoListCtrl'
+		})
+		.when('/ingos/:id', {
+			templateUrl: 'partials/ingoDetailView.html',
+			controller: 'ingoDetailCtrl'
+		})
+		.when('/ingoBy/:id', {
+			templateUrl: 'partials/ingoByView.html',
+			controller: 'ingoByCtrl'
+		})
+		.when('/ingoBy/:type/:recherche', {
+			templateUrl: 'partials/ingoListView.html',
+			controller: 'ingoListCtrl'
+		})
+		.when('/sign/:io', {
+			templateUrl: 'partials/ingoSignInView.html',
+			controller: 'ingoSignInCtrl'
+		})
+		.when('/test', {
+			templateUrl: 'partials/testView.html',
+			controller: 'testCtrl'
+		})
+		.otherwise({
+			redirectTo: '/'
+		});
+		
 }]);
 
-/* Filters */
-app.filter('website', function() {
-    return function(input) {
-        
-        var output = '';
-        
-        if (input.indexOf('http://') != -1) { // si l'input contient http://
-            output = '<a href="' + input + '" target="_blank">' + input + '</a>';
-        } else {
-            output = '<a href="http://' + input + '" target="_blank">' + input + '</a>';
-        }
-        
-        return output; 
-    }
+/* filters */
+app.filter('website',function(){
+  
+ return function(input){  // closure
+   var result="";
+   if (input.indexOf('http://') == -1)
+      result = 'http://' + input ;
+   else result = input;
+   return '<a target="site" href="' + result + '">'+input+'</a>';
+ };
+ 
 });
 
-app.filter('email', function() {
-    return function(input) {
-        
-        var output = '';
-        
-        output = '<a href="mailto:' + input + '">' + input + '</a>';
-        
-        return output; 
-    }
-});
-
-
-/* Directives */
-app.directive('composeEmail', function() {
+/* directives */
+/* 
+correspond au my-dir du view
+attention à la caste : ici il y a une majuscule ! 
+*/
+app.directive('myDir', function(){
+  
   return {
-    restrict: 'EA',
-    replace: true,
-    scope: true,
-    /*controllerAs: 'compose',
-    controller: function () {
-
+    restrict: 'A', // restrint le domaine de création de directive  à un element (E) ou un attribut  (A) ou aux deux (EA)
+    //template: '<span>ma 1ere directive {{ message }} </span>'
+    templateUrl: 'partials/template.html',
+    replace: true, // remplace la directive par ce qu'il y a avant (par defaut false -> ajoute)
+    scope: {
+      player: '='
     },
-    */
-    link: function ($scope, $element, $attrs) {
-
-    },
-    template: [
-      '<div class="compose-email">',
-        '<input type="text" placeholder="To..." ng-model="compose.to">',
-        '<input type="text" placeholder="Subject..." ng-model="compose.subject">',
-        '<textarea placeholder="Message..." ng-model="compose.message"></textarea>',
-      '</div>'
-    ].join('')
+    link: function($scope, element, attrs){ 
+      // attrx récupère ce qu'il y a dans l'attribut de la balise (ex : joueur dans player=)
+      // element permet de faire du jquery dans la balise -> https://docs.angularjs.org/api/ng/function/angular.element
+      element.children('button').bind('click', function(){
+        console.log(attrs);
+      });
+      var valeur = element.find('p').text();
+      element.find('p').replaceWith('<input type="text" value="' + valeur + '">');
+    }
   };
+  
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- ingoApp/js/controllers/mainCtrl.js
+++ ingoApp/js/controllers/mainCtrl.js
@@ -1,55 +1,189 @@
 var ingoControllers = angular.module('ingoControllers', []);
 
-ingoControllers.controller('ingoListCtrl', ['$scope', '$http', 'ingoFactory', 'ingoDataFactory',
-    function($scope, $http, ingoFactory, ingoDataFactory) {
 
-        $scope.committees = ingoFactory.committees;
-        $scope.georeps = ingoFactory.georeps;
-        $scope.aocs = ingoFactory.aocsTransformed('----');
+ingoControllers.controller('menuCtrl', ['$rootScope', 'ingoInterface', function($rootScope, ingoInterface){
+  
+  $rootScope.isSessionOpen = false;
+  $rootScope.menu = ingoInterface.menu;
+  
+}]);
 
-
-        if (ingoDataFactory.ingos === false) { // ingos jamais chargées dans le factory
-            ingoDataFactory.getIngos().then(function(data) {
-                $scope.ingos = data;
-            })
-        } else {
-            $scope.ingos = ingoDataFactory.ingos;
-        }
-
+ingoControllers.controller('ingoListCtrl', ['$scope', 'ingoFactory', 'ingoDataFactory', '$routeParams', function($scope, ingoFactory, ingoDataFactory, $routeParams){
+  
+  $scope.choix = true;
+  if($routeParams.type !== undefined){
+    $scope.choix = false;
+    var type = $routeParams.type;
+    $scope.lien = type;
+    var recherche = $routeParams.recherche;
+    $scope.titre = recherche;
+    //coche la case faisant apparaitre les filtres
+    $scope.chkFilters = true;
+    switch(type){
+      case 'country':
+        $scope.selectGeoRep = recherche;
+        break;
+      case 'committee':
+        $scope.selectCommittee = recherche;
+        break;
+      case 'aoc':
+        $scope.selectAoc = recherche;
+        break;
     }
-]);
+  }
+  
+  
+  $scope.aocs = ingoFactory.aocsTransformed('--');
+  $scope.committees = ingoFactory.committees;
+  $scope.geoReps = ingoFactory.geoReps;
+  
+  if(!ingoDataFactory.ingos){
+    ingoDataFactory.getIngos().then(//promise
+      function(data){//success
+        $scope.ingos = data;
+      }
+    );
+  }else{
+        $scope.ingos = ingoDataFactory.ingos;
+  }
+  
+}]);
 
+ingoControllers.controller('ingoDetailCtrl', ['$scope', '$routeParams', 'ingoDataFactory', function($scope, $routeParams, ingoDataFactory){
+  
+  var id = parseInt($routeParams.id);
+  $scope.id = id;
+        
+  if (!ingoDataFactory.ingos) { // ingos jamais chargées dans le factory
+      ingoDataFactory.getIngos().then(
+        function(data) {
+          $scope.ngo = ingoDataFactory.getIngo(id);
+      }
+    );
+  } else {
+      $scope.ngo = ingoDataFactory.getIngo(id);
+  }
 
-ingoControllers.controller('ingoDetailCtrl', ['$scope', '$routeParams', 'ingoDataFactory',
-    function($scope, $routeParams, ingoDataFactory) {
+}]);
 
-        var id = parseInt($routeParams.id);
-
-        if (ingoDataFactory.ingos === false) { // ingos jamais chargées dans le factory
-            ingoDataFactory.getIngos().then(function(data) {
-                $scope.ngo = ingoDataFactory.getIngo(id);
-            });
-        } else {
-            $scope.ngo = ingoDataFactory.getIngo(id);
+ingoControllers.controller('ingoByCtrl', ['$scope', '$routeParams', 'ingoFactory', function($scope, $routeParams, ingoFactory){
+  
+  var id = $routeParams.id;
+  $scope.id = id;
+  switch(id){
+    case 'country':
+      $scope.titre = "Country";
+      // pour _.each voir underscore.js qui permet de faire plein de chose
+      var row = '<div class="row">';
+      _.each(ingoFactory.geoReps, function(geoRep, i){
+        if(i % 4 === 0){
+          row += '</div><div class="row">';
         }
-
-    }
-]);
-
-ingoControllers.controller('ingoSignInCtrl', ['$scope', '$routeParams', '$http',
-    function($scope, $routeParams, $http) {
-      $scope.submit = function(){
-          //console.log($scope.user.email);
-          var server = 'http://test2-opusidea.c9.io/ingo/sign-in';
-          var user = angular.toJson($scope.user);
-          $http.post(server, user)
-          .success(function(data) {
-            $scope.message = 'Identification réussie';
-          })
-          .error(function(status){
-            $scope.message = 'Problème';
+        row += '<div class="three columns"><h2><a href="#/ingoBy/country/' + geoRep + '">' + geoRep + '</a></h2></div>';
+        i++;
+      });
+      row += '</div>';
+      $scope.row = row;
+      break;
+    case 'committee':
+      $scope.titre = "Committee";
+      // pour _.each voir underscore.js qui permet de faire plein de chose
+      var row = '<div class="row">';
+      _.each(ingoFactory.committees, function(committee, i){
+        if(i % 3 === 0){
+          row += '</div><div class="row">';
+        }
+        row += '<div class="four columns"><h2><a href="#/ingoBy/committee/' + committee + '">' + committee + '</a></h2></div>';
+        i++;
+      });
+      row += '</div>';
+      $scope.row = row;
+      break;
+    case 'aoc':
+      $scope.titre = "area of competence";
+      // pour _.each voir underscore.js qui permet de faire plein de chose
+      var row = '<div class="row">';
+      var tabAoc = [];
+      _.each(ingoFactory.aocs, function(aocs, i){
+        var objet = _.keys(aocs).toString();  
+        if(i % 2 === 0){
+          row += '</div><div class="row">';
+        }
+        row += '<div class="six columns"><h2><a href="#/ingoBy/aoc/' + objet + '">' + objet + '</a>';
+        
+        
+        _.each(aocs[objet], function(i){
+            row += '<br/><a href="#/ingoBy/aoc/' + i + '">   .   ' + i + '</a>';
           });
+        row += '</h2></div>';
+        i++;
+      });
+      row += '</div>';
+      $scope.row = row;
+      break;
+  }
+  
+}]);
 
+ingoControllers.controller('ingoSignInCtrl', ['$scope', '$routeParams', '$http', '$rootScope', function($scope, $routeParams, $http, $rootScope){
+  
+  console.log("msg : " + $scope.msg);
+  // si son scope ne le trouve pas il le cherche dans le root
+  console.log("msg : " + $scope.msg2);
+  console.log("msg : " + $rootScope.msg2);
+  
+  $scope.submit = function(){
+  var server = 'http://test2-opusidea.c9.io/ingo/sign-in';
+  var user = angular.toJson($scope.user);
+  $http.post(server, user)
+    .success(function(data){
+      console.log(data);
+      $scope.message = "Identification réussie";
+      $rootScope.isSessionOpen = true;
+      $rootScope.menu.signIn.text = 'SIGN OUT';
+      $rootScope.menu.signIn.url = 'sign/out';
+    })
+    .error(function(status){
+      $scope.message = "Problème " + status;
+      
+    });
   };
-}
-]);
+  
+}]);
+
+ingoControllers.controller('testCtrl', ['$scope', '$rootScope', '$interval', function($scope, $rootScope, $interval){
+  $scope.msg = 'bravo';
+  $rootScope.msg2 = 'fantastique';
+  console.log("msg : " + $scope.msg);
+  console.log("msg : " + $rootScope.msg2);
+  
+  $scope.message = "Angular";
+  $scope.joueurs = [
+      {
+        name: 'Lionel Messi',
+        numero: 10,
+        image:''
+      },
+      {
+        name: 'Hugo Lloris',
+        numero: 1,
+        image:''
+      },
+      {
+        name: 'Cristiano Ronaldo',
+        numero: 7,
+        image:''
+      }
+    ];
+  
+  images = ['http://www.ac-grenoble.fr/ien.vienne1-2/spip/IMG/bmp_Image004.bmp',
+    'http://www.online-image-editor.com//styles/2014/images/example_image.png',
+    'https://www.wonderplugin.com/wp-content/plugins/wonderplugin-lightbox/images/demo-image0.jpg'];
+  var i = 0;
+  $scope.source = images[i];
+  $interval(function() {
+    if (i == 2) { i = 0; }else{ i++; }
+    $scope.source = images[i];
+  }, 3000);
+  
+}]);
